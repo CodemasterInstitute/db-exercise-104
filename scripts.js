e03f45c866a45b4a0f68eb9b89f240117284ad6a
Daniel Boterhoven <hello@danielboterhoven.tech>
2016-05-05 18:47:03 +0800

    inital solution commit

--- scripts.js
+++ scripts.js
@@ -24,21 +24,85 @@ window.onload = function() {
 
 
     // load footer
+    var footerSection = document.querySelector('#footer');
+    footerSection.innerHTML = 'Loading...'
 
+    $.get({
+        url: 'partials/footer.html',
+        success: onLoadedFooterPartial
+    });
+
+    function onLoadedFooterPartial(data, textStatus, jqXHR) {
+        footerSection.innerHTML = data;
+
+    }
 
 
     // INTERMEDIATE exercise
 
     // write your solution here!
 
+    var tabContent = document.querySelector('#tabContent');
+    var tabMenuItems = document.querySelectorAll('.tabs-menu ul li');
+
+    for (var i = 0; i < tabMenuItems.length; i++) {
+        var tabMenuItem = tabMenuItems[i];
+
+        tabMenuItem.onclick = onMenuItemClick
+
+        if (i === 0) {
+            tabMenuItem.click();
+        }
+    }
+
+    function onMenuItemClick(event) {
+        event.preventDefault();
+
+        // get the target url we want o load the content from
+        var contentUrl = this.getAttribute('data-content-url');
+
+        // display the 'loading' indicator in the content area
+        tabContent.classList.add('loading');
+
+        $.get({
+            url: contentUrl,
+            success: onContentPartial
+        });
+
+        function onContentPartial(data, textStatus, jqXHR) {
+            // update the markup in the content area
+            tabContent.innerHTML = data;
+
+            // remove the 'loading' indicator from the content area
+            tabContent.classList.remove('loading');
+        }
+    }
+
 }
 
 // EXPERT exercise
-// $ = {};
-// $.get = vanillaGet;
+$ = {};
+$.get = vanillaGet;
 
-// function vanillaGet(request) {
+function vanillaGet(request) {
 
-//     // write your solution here!
+    // write your solution here!
 
-// }
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', request.url, true);
+
+    xhr.send();
+
+    xhr.onreadystatechange = processRequest;
+
+    function processRequest(e) {
+
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            
+            request.success(xhr.responseText, xhr.statusText);
+
+        }
+
+    }
+}
